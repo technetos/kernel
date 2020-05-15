e67f5b6df7093b3981abb9b8a0ca2c2483fc9767
technetos <mrgould93@gmail.com>
2020-05-15 17:13:06 -0400

    Working on color codes

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+// The PanicInfo struct contains the line number and file where the panic occured.  This function
+// never returns, it loops forever.  
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello world!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // This is a raw pointer.
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; 
+        }
+    }
+
+    loop {}
+}
